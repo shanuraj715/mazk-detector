e1df75b2e3d23aee5e7da4ee8179e5fdfb71cdf4
amansandhu2718 <77589604+amansandhu2718@users.noreply.github.com>
2021-05-24 12:56:39 +0530

    .

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -21,7 +21,7 @@ class Home extends Component {
         <section>
           <div className="container mainsection">
             <div className="row">
-              <div className="col col-12 col-lg-6">
+              <div className="col col-12 col-lg-6 ">
                 <div className="writings my-auto">
                   <h1 className="mainheading">Mask Detector</h1>
                   <h5 className="secondheading">
@@ -33,13 +33,9 @@ class Home extends Component {
                   </button>
                 </div>
               </div>
-              <div className="col col-12 col-lg-6">
+              <div className="col col-12 col-lg-6 ">
                 <div className="imgmain">
-                  <img
-                    src={Mainbg}
-                    className="img-fluid"
-                    alt="..."
-                  />
+                  <img src={Mainbg} className="img-fluid homecol" alt="..." />
                 </div>
               </div>
             </div>
